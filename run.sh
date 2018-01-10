0331cb53c2cb4907ad090afc3be60acb288925d1
Snorri Sturluson <snorri.sturluson@ccpgames.com>
2018-01-10 16:12:59 +0000

    Adding node name when running locally.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-EJABBERD_CONFIG_PATH=ejabberd.yml erl -pa ebin -pa deps/*/ebin -pa test -pa deps/elixir/lib/*/ebin/ -s ejabberd
+EJABBERD_CONFIG_PATH=ejabberd.yml erl -sname ejabberd -pa ebin -pa deps/*/ebin -pa test -pa deps/elixir/lib/*/ebin/ -s ejabberd
